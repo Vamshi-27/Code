a4c73d45eeebe667f77c31ceda1f044f0f9551ff
Vamshi-27 <vamshira18@outlook.com>
2024-02-20 21:36:53 +0530

    sync

---
+++ Java/Introduction/Demo.java
@@ -0,0 +1,13 @@
+package Java.Introduction;
+
+public class Demo
+{
+    public static void main(String[] args) 
+    {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum=" + sum);
+        
+    }
+}

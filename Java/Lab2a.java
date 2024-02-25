1f0055aea09b3aee493e39f27bdb1fc50676a5ae
Vamshi-27 <vamshira18@outlook.com>
2024-02-25 09:14:22 +0530

    sync

---
+++ Java/Lab2a.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Lab2a
+{
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println("Enter the operator to perform the operation:");
+        char op=s.next().charAt(0);
+        int c=0;
+        switch(op)
+        {
+            case '+':
+                c=a+b;
+                break;
+            case '-':
+                c=a-b;
+                break;
+            case '*':
+                c=a*b;
+                break;
+            case '/':
+                if(b!=0)
+                    c=a/b;
+                else
+                {
+                    System.out.println("Division by zero is not possible");
+                    System.exit(0);
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                System.exit(0);
+        }
+        System.out.println("Result:"+a+op+b+"="+c);
+        s.close();
+    }
+}

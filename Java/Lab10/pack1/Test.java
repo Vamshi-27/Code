2301dd29fba126c95c324d1822f74244daf9f844
Vamshi-27 <vamshira18@outlook.com>
2024-02-25 15:09:48 +0530

    sync

---
+++ Java/Lab10/pack1/Test.java
@@ -0,0 +1,12 @@
+package pack1;
+import mypack.Example;
+
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        Example e=new Example();
+        e.set();
+        e.display();
+    }
+}

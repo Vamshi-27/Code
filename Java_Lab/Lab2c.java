f61ac16eb7c1ba81b35371facaa59287aa796de2
Vamshi-27 <vamshira18@outlook.com>
2024-02-25 09:43:22 +0530

    sync

--- Java_Lab/Lab2c.java
+++ Java_Lab/Lab2c.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
 public class Lab2c
 {
-    
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int sum = 0, count = 0;
+        int a[] = new int[10];
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < 10; i++)
+        {
+            a[i] = s.nextInt();
+        }
+        for (int x : a)
+        {
+            sum += x;
+            count++;
+            if (count == 4)
+                break;
+        }
+        System.out.println("Sum of first four elements in the array is " + sum);
+        s.close();
+    }
 }
---
+++ Java_Lab/Lab3.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+class Employee
+{
+    String eid, name;
+    double salary;
+
+    void read() 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter ID,Name and Salary");
+        eid = s.next();
+        name = s.next();
+        salary = s.nextDouble();
+        s.close();
+    }
+
+
+    void display() 
+    {
+        System.out.println(eid + "\t" + name + "\t" + salary);
+    }
+
+
+    void raiseSalary(double per)
+    {
+        salary = salary + (per / 100) * salary;
+    }
+
+}
+
+public class Lab3
+{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the number of employees:");
+        int n = s.nextInt();
+        Employee e[] = new Employee[n];
+        for (int i = 0; i < n; i++) 
+        {
+            e[i] = new Employee();
+            e[i].read();
+        }
+        System.out.println("Emp ID\tEmp Name\tEmp Salary\n");
+        for (int i = 0; i < n; i++)
+        {
+            e[i].display();
+        }
+        System.out.println("Enter percentage:");
+        double p = s.nextDouble();
+        for (int i = 0; i < n; i++)
+        {
+            e[i].raiseSalary(p);
+        }
+        System.out.println("ID\tName\tSalary");
+        for (int i = 0; i < n; i++)
+        {
+            e[i].display();
+        }
+        s.close();
+    }
+}

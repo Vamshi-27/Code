a85ab4d6afaef160880c8b203b8068505d63a5c6
Vamshi-27 <vamshira18@outlook.com>
2024-03-17 07:37:47 +0530

    sync

---
+++ Java_Code/Constructor.java
@@ -0,0 +1,21 @@
+//Constructor->Special method to initialize the instance variables.
+
+public class Constructor
+{;
+    int a,b,c;
+    Constructor()
+    {
+        a=10;
+        b=20;
+        c=30;
+    }
+    void display()
+    {
+        System.out.println("a = "+a+"\nb = "+b+"\nc = "+c);
+    }
+    public static void main(String[] args)
+    {
+        Constructor c=new Constructor();
+        c.display();
+    }
+}

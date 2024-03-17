f1bb945740a6d56458d8a380d38dcf1bb8184389
Vamshi-27 <vamshira18@outlook.com>
2024-03-17 07:34:07 +0530

    sync

---
+++ Java_Code/Method.java
@@ -0,0 +1,27 @@
+//Method Overloading
+
+public class Method 
+{
+    void add(int a, int b)
+    {
+        int sum=a+b;
+        System.out.println("Sum = "+sum);
+    }
+    void add(int a, int b, int c)
+    {
+        int sum=a+b+c;
+        System.out.println("Sum = "+sum);
+    }
+    void add(double a,double b)
+    {
+        double sum=a+b;
+        System.out.println("Sum = "+sum);
+    }
+    public static void main(String[] args)
+    {
+        Method obj=new Method();
+        obj.add(10,20);
+        obj.add(10,20,30);
+        obj.add(1.414,1.732);
+    }
+}

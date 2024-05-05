81fa18cd25ec170bf308c7fc73152ca0015cee82
Vamshi-27 <vamshira18@outlook.com>
2024-05-05 15:10:04 +0530

    sync

---
+++ Java_Code/StackDemo.java
@@ -0,0 +1,14 @@
+package Java_Code;
+
+import java.util.Stack;
+
+public class StackDemo
+{
+    public static void main(String[] args) 
+    {
+        Stack<Integer> list = new Stack<>();
+        list.push(3);
+        list.push(20);
+        System.out.println(list);
+    }
+}
